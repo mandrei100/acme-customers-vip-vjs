ef2dda313dae02103c3e96b9c6a7046a3f5d1366
mandrei100 <mcrisandreica@hotmail.com>
2019-10-01 08:54:03 -0400

    Hey man! Made some changes to the file, but I finally got the name, email, and VIP checkbox to work

--- script.js
+++ script.js
@@ -4,39 +4,81 @@ const emailInput = document.querySelector('input[name=email]');
 const vipInput = document.querySelector('input[type=checkbox]');
 const errorMessage = document.querySelectorAll('.error');
 const button = document.querySelector('.btn')
+const nameList = document.querySelector('.nameList')
 
-let newCustomer = {};
+
+const newCustomer = {};
 let vipArray = [];
 
+vipInput.addEventListener('click', ev=> {
+    console.log('checked ', ev.target.checked);
+})
+
+
 formInput.addEventListener('keyup', ev => {
-    console.log(errorMessage);
-    if(!ev.target.value) {
-        errorMessage[0].innerHTML = 'name required'
-        errorMessage[1].innerHTML = 'email required'
-    } else {
-        errorMessage[0].innerHTML = ''
-        errorMessage[1].innerHTML = ''
+    // create and object {name: '', value: ''}
+    const { target: {name, value} } = ev;
+    console.log(ev.target.name, ev.target.value);
+    if (name) {
+        newCustomer[name] = value;
     }
-    ev.preventDefault();
-    console.log(nameInput.value);
-    let newCustomer = {
-        name: nameInput.value,
-        email: emailInput.value,
-        isVip: vipInput.checked
-    };
-    
     console.log(newCustomer);
 })
 
 
-
-
-button.addEventListener('submit', ev =>{
+formInput.addEventListener('submit', ev => {
     ev.preventDefault();
+    // console.log(errorMessage);
+    //  if(!ev.target.value) {
+    //      errorMessage[0].innerHTML = 'name required'
+    //      errorMessage[1].innerHTML = 'email required'
+    //  } else {
+    //      errorMessage[0].innerHTML = ''
+    //      errorMessage[1].innerHTML = ''
+    //  }
+    // console.log(ev.target.name, ev.target.value)
+    //  for (let i = 0; i < ev.target.elements.length; i++){
+    //      const currentEl = ev.target.elements[i];
+    //      console.log(currentEl.name, currentEl.value);
+    //  }
+    
+    //  console.log(newCustomer);
+})
+
+
+// const newElement = () =>{
+//     let li = document.createElement('li');
+//     let input = `${nameInput.value} ${emailInput.value} ${errorMessage.value}`;
+//     li.appendChild(input);
+//     let destroyButton = document.elementFromPoint('button');
+//     var destroyButtonText = document.createTextNode("Destroy >=)");
+//     destroyButton.className = "close";
+//     destroyButton.appendChild(destroyButtonText);
+//     li.appendChild(destroyButton);
+
+    // ev.preventDefault();
+    // console.log(nameInput.value);
+    // let newCustomer = {
+    //     name: nameInput.value,
+    //     email: emailInput.value,
+    //     isVip: vipInput.checked
+    // };
+
+
+
+//     for (var i = 0; i < close.length; i++) {
+//         close[i].onclick = function(){
+//             namelist = this.parentElement;
+//         }
+//     }
+// }
+
+// button.addEventListener('submit', ev =>{
+//     ev.preventDefault();
     
 
-    vipArray.push()
+//     vipArray.push()
     
 
-    console.log(newCustomer);
-} )
+//     console.log(newCustomer);
+// } )
