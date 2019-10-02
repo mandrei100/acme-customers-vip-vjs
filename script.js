9126fd12af642a508a170352cd4bd8b1bd2f5a14
mandrei100 <mcrisandreica@hotmail.com>
2019-10-02 08:33:36 -0400

    Added and deleted some things

--- script.js
+++ script.js
@@ -3,21 +3,47 @@ const nameInput = document.querySelector('input[name=name]');
 const emailInput = document.querySelector('input[name=email]');
 const vipInput = document.querySelector('input[type=checkbox]');
 const errorMessage = document.querySelectorAll('.error');
-const button = document.querySelector('.btn')
-const nameList = document.querySelector('.nameList')
+const button = document.querySelector('.btn');
+const nameList = document.querySelector('.nameList');
 
 
 const newCustomer = {};
-let vipArray = [];
+const customerArray = [];
+const vipArray = [];
 
-vipInput.addEventListener('click', ev=> {
-    console.log('checked ', ev.target.checked);
+console.log(customerArray);
+
+formInput.addEventListener("keyup", ev =>{
+    if(!ev.target.value) {
+        errorMessage[0].innerHTML = 'name required'
+        errorMessage[1].innerHTML = 'email required'
+    } else {
+        errorMessage[0].innerHTML = ''
+        errorMessage[1].innerHTML = ''
+    }
 })
 
 
-formInput.addEventListener('keyup', ev => {
+vipInput.addEventListener('click', ev=> {
+    console.log('checked ', ev.target.checked);
+    
+    const {target: {name, checked}} = ev;
+    if (checked) {
+        newCustomer[name] = checked;
+    }
+    customerArray.push(newCustomer);
+    console.log(customerArray);
+    
+    console.log(newCustomer);
+})
+
+
+formInput.addEventListener('submit', ev => {
     // create and object {name: '', value: ''}
     const { target: {name, value} } = ev;
+    // prior to destructuring
+    // const evName = ev.target.name;
+    // if (ev.target.name)
     console.log(ev.target.name, ev.target.value);
     if (name) {
         newCustomer[name] = value;
@@ -28,57 +54,11 @@ formInput.addEventListener('keyup', ev => {
 
 formInput.addEventListener('submit', ev => {
     ev.preventDefault();
-    // console.log(errorMessage);
-    //  if(!ev.target.value) {
-    //      errorMessage[0].innerHTML = 'name required'
-    //      errorMessage[1].innerHTML = 'email required'
-    //  } else {
-    //      errorMessage[0].innerHTML = ''
-    //      errorMessage[1].innerHTML = ''
-    //  }
-    // console.log(ev.target.name, ev.target.value)
-    //  for (let i = 0; i < ev.target.elements.length; i++){
-    //      const currentEl = ev.target.elements[i];
-    //      console.log(currentEl.name, currentEl.value);
-    //  }
-    
-    //  console.log(newCustomer);
 })
 
+const render = () => {
+    newCustomer.form
+    const text = `${newCustomer.name}`;
+    nameList.innerHTML = text;
 
-// const newElement = () =>{
-//     let li = document.createElement('li');
-//     let input = `${nameInput.value} ${emailInput.value} ${errorMessage.value}`;
-//     li.appendChild(input);
-//     let destroyButton = document.elementFromPoint('button');
-//     var destroyButtonText = document.createTextNode("Destroy >=)");
-//     destroyButton.className = "close";
-//     destroyButton.appendChild(destroyButtonText);
-//     li.appendChild(destroyButton);
-
-    // ev.preventDefault();
-    // console.log(nameInput.value);
-    // let newCustomer = {
-    //     name: nameInput.value,
-    //     email: emailInput.value,
-    //     isVip: vipInput.checked
-    // };
-
-
-
-//     for (var i = 0; i < close.length; i++) {
-//         close[i].onclick = function(){
-//             namelist = this.parentElement;
-//         }
-//     }
-// }
-
-// button.addEventListener('submit', ev =>{
-//     ev.preventDefault();
-    
-
-//     vipArray.push()
-    
-
-//     console.log(newCustomer);
-// } )
+}
